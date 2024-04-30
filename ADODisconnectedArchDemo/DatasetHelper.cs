45d270efefac079d59aa3f3698045ea720abc631
Ranga Sai <19BQ1A04C8@vvit.net>
2024-04-30 17:55:16 +0530

    Add project files.

---
+++ ADODisconnectedArchDemo/DatasetHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ADODisconnectedArchDemo
+{
+    internal class DatasetHelper
+    {
+        SqlConnection? connect = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PRODUCTDB;Integrated Security=True;Pooling=False");
+        DataSet dataset = new DataSet();
+        SqlDataAdapter adapter;
+        SqlCommandBuilder? command;
+       
+        public DataSet fetchData()
+        {
+            
+            string query = "SELECT * FROM PRODUCT";
+            adapter = new SqlDataAdapter(query,connect);
+            command = new SqlCommandBuilder(adapter);
+            adapter.Fill(dataset);
+            return dataset;
+        }
+
+
+        public void  modifyData()
+        {
+            adapter.Update(dataset);
+        }
+    }
+}
